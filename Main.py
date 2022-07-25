1df244701ca8780ebabd71a957932905ad40e79a
JenefaNadar <109951988+JenefaNadar@users.noreply.github.com>
2022-07-25 16:03:39 +0530

    Final Commit

--- Main.py
+++ Main.py
@@ -1,31 +1,32 @@
 import os
 class Stack:
-    """The Class Implementing A Stack"""
+    #The Class Implementing A Stack
     def __init__(self, size):
-        """  """
+        #Initializing stack with size attribute
         self.items = []
         self.size = size
 
     def is_empty(self):
-    """It returns true if the stack is empty and false if the stack isn't empty"""
+        
+        #It returns true if the stack is empty and false if the stack isn't empty
         return len(self.items) == 0
 
-    def is_full(self):
-    """It returns true if the stack is full and false if the stack isn't full"""
+    def is_full(self):      
+        #It returns true if the stack is full and false if the stack isn't full
        return len(self.items) == self.size
 
-    def push(self, data):
-        """Pushes data into the stack"""
+    def push(self, data):        
+        #Pushes data into the stack
         if not self.is_full():
             self.items.append(data)
 
     def pop(self):
-        """Deletes data from the stack"""
+        #Deletes data from the stack
         if not self.is_empty():
           self.items.pop(-1)
 
     def status(self):
-        """Displays elements/data that are currently present in the stack"""
+        #Displays elements/data that are currently present in the stack
         for elem in self.items:
                   print(elem)
 
